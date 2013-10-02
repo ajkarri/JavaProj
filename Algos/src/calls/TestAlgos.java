b5bce0a2aa19f38238c704dce32beb87d70d1f07
ajkarri <karri.ajay@hotmail.com>
2013-10-02 11:28:07 -0400

    push

--- Algos/src/calls/TestAlgos.java
+++ Algos/src/calls/TestAlgos.java
@@ -1,13 +1,41 @@
 package calls;
 import algos.*;
+import linkedList.*;
 public class TestAlgos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TestBoyerMooreVoteAlgo();
+		TestSingleLinkedList();
 
 	}
-
+	
+	public static void TestSingleLinkedList() {
+		SingleLinkedList ll = new SingleLinkedList(5);
+		ll.InsertNodeAtEnd(4);
+		ll.InsertNodeAtEnd(6);
+		ll.InsertNodeAtEnd(7);
+		ll.InsertNodeAtEnd(8);
+		ll.InsertAtTheBegining(10);
+		ll.InsertNodeAt(12, 4);
+		System.out.println("length of the linked list " + ll.length());
+		System.out.println("node at 5 is " + ll.NodeAt(5).getData());
+		
+		
+		ll.PrintLinkedList();
+		ll.RemoveNodeAt(4);
+		ll.RemoveNodeAtBeginning();
+		ll.RemoveNodeAtEnd();
+		ll.PrintLinkedList();
+		ll.RemoveNode(7);
+		ll.PrintLinkedList();
+		ll.RemoveNode(5);
+		ll.PrintLinkedList();
+		ll.RemoveNode(4);
+		ll.PrintLinkedList();
+		ll.RemoveNode(6);
+		ll.PrintLinkedList();
+		ll.RemoveNode(4);
+	}
 	public static void TestBoyerMooreVoteAlgo () {
 		int [] TestArray = {1,1,1,3,3,2,2,3,3,3,2,3,3};
 		BoyerMooreVoteAlgo boyer = new BoyerMooreVoteAlgo(TestArray);
